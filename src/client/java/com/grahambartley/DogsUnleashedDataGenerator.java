4d95bb17e94a9ae3e871f6af7007eea8c17c1199
Graham Bartley <grabartley@gmail.com>
2025-10-09 22:24:38 +0100

    Fabric mod template (#1)

---
+++ src/client/java/com/grahambartley/DogsUnleashedDataGenerator.java
@@ -0,0 +1,9 @@
+package com.grahambartley;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DogsUnleashedDataGenerator implements DataGeneratorEntrypoint {
+  @Override
+  public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {}
+}
