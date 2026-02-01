c3e7c2ce1c33da46cddfb31b181721d73e936e9b
Graham Bartley <grabartley@gmail.com>
2026-02-01 23:29:41 +0000

    refactor: Remove redundant interface declarations from entity subclasses

--- src/main/java/com/grahambartley/entity/GoldenRetrieverEntity.java
+++ src/main/java/com/grahambartley/entity/GoldenRetrieverEntity.java
@@ -5,13 +5,11 @@ import static com.grahambartley.ModEntities.GOLDEN_RETRIEVER;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
-import net.minecraft.entity.mob.Angerable;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.World;
-import software.bernie.geckolib.animatable.GeoEntity;
 
-public class GoldenRetrieverEntity extends UnleashedDogEntity implements GeoEntity, Angerable {
+public class GoldenRetrieverEntity extends UnleashedDogEntity {
 
   public static DefaultAttributeContainer.Builder createAttributes() {
     return MobEntity.createMobAttributes()
