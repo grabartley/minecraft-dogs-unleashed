313b87befe00513a1d0913995c477bfd5e9f0a44
Graham Bartley <grabartley@gmail.com>
2026-04-04 17:18:01 +0100

    feat: husky howling

--- src/main/java/com/grahambartley/ModConstants.java
+++ src/main/java/com/grahambartley/ModConstants.java
@@ -55,12 +55,14 @@ public class ModConstants {
   public static final int SHIBA_INU_SPAWN_MAX_GROUP = 2;
 
   public static final int BARK_COOLDOWN_TICKS = 6 * MINECRAFT_TICK_RATE;
-  public static final int RANDOM_BARK_CHANCE = 600;
+  public static final int RANDOM_BARK_CHANCE = 800;
   public static final int HOWL_COOLDOWN_TICKS = 30 * MINECRAFT_TICK_RATE;
+  public static final int RANDOM_HOWL_CHANCE = 4000;
+  public static final int HOWL_DURATION_TICKS = (int) (4.5f * MINECRAFT_TICK_RATE);
   public static final float LOW_HEALTH_THRESHOLD = 0.3f;
   public static final float BARK_VOLUME = 1.0f;
   public static final float BARK_PITCH = 1.0f;
   public static final float HOWL_VOLUME = 1.5f;
   public static final float HOWL_PITCH = 1.0f;
-  public static final int NEAR_FULL_MOON_THRESHOLD = 1;
+  public static final int FULL_MOON_PHASE = 0;
 }
--- src/main/java/com/grahambartley/entity/HuskyEntity.java
+++ src/main/java/com/grahambartley/entity/HuskyEntity.java
@@ -1,23 +1,36 @@
 package com.grahambartley.entity;
 
+import static com.grahambartley.ModConstants.FULL_MOON_PHASE;
 import static com.grahambartley.ModConstants.HOWL_COOLDOWN_TICKS;
+import static com.grahambartley.ModConstants.HOWL_DURATION_TICKS;
 import static com.grahambartley.ModConstants.HOWL_PITCH;
 import static com.grahambartley.ModConstants.HOWL_VOLUME;
-import static com.grahambartley.ModConstants.NEAR_FULL_MOON_THRESHOLD;
+import static com.grahambartley.ModConstants.RANDOM_HOWL_CHANCE;
 import static com.grahambartley.ModEntities.HUSKY;
 
 import com.grahambartley.ModSounds;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.data.DataTracker;
+import net.minecraft.entity.data.TrackedData;
+import net.minecraft.entity.data.TrackedDataHandlerRegistry;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.world.World;
+import software.bernie.geckolib.animation.AnimatableManager;
+import software.bernie.geckolib.animation.AnimationController;
+import software.bernie.geckolib.animation.PlayState;
+import software.bernie.geckolib.animation.RawAnimation;
 
 public class HuskyEntity extends UnleashedDogEntity {
 
+  private static final TrackedData<Boolean> HOWLING =
+      DataTracker.registerData(HuskyEntity.class, TrackedDataHandlerRegistry.BOOLEAN);
+
   private int howlCooldownTicks = 0;
+  private int howlActiveTicks = 0;
 
   public static DefaultAttributeContainer.Builder createAttributes() {
     return MobEntity.createMobAttributes()
@@ -28,6 +41,12 @@ public class HuskyEntity extends UnleashedDogEntity {
 
   public HuskyEntity(EntityType<? extends UnleashedDogEntity> entityType, World world) {
     super(entityType, world);
+  }
+
+  @Override
+  protected void initDataTracker(DataTracker.Builder builder) {
+    super.initDataTracker(builder);
+    builder.add(HOWLING, false);
   }
 
   @Override
@@ -50,26 +69,65 @@ public class HuskyEntity extends UnleashedDogEntity {
     return ModSounds.HUSKY_BARK;
   }
 
+  public boolean isHowling() {
+    return this.dataTracker.get(HOWLING);
+  }
+
+  private void setHowling(boolean howling) {
+    this.dataTracker.set(HOWLING, howling);
+  }
+
   public int getHowlCooldownTicks() {
     return this.howlCooldownTicks;
   }
 
-  private boolean canHowl() {
+  private boolean isHowlConditionMet() {
     return !this.isDead()
         && !this.isSleepingInBed()
-        && this.howlCooldownTicks <= 0
         && !this.getWorld().isDay()
-        && this.getWorld().getMoonPhase() <= NEAR_FULL_MOON_THRESHOLD;
+        && this.getWorld().getMoonPhase() == FULL_MOON_PHASE;
+  }
+
+  private boolean canHowl() {
+    return isHowlConditionMet() && this.howlCooldownTicks <= 0;
   }
 
   @Override
   protected void tickBreedSpecificSounds() {
-    if (this.howlCooldownTicks > 0) {
-      this.howlCooldownTicks--;
+    if (this.howlCooldownTicks > 0) this.howlCooldownTicks--;
+
+    if (this.howlActiveTicks > 0) {
+      this.howlActiveTicks--;
+      if (this.howlActiveTicks == 0) {
+        this.setHowling(false);
+      }
     }
-    if (this.canHowl() && this.random.nextInt(600) == 0) {
+
+    boolean willHowl = this.canHowl() && this.random.nextInt(RANDOM_HOWL_CHANCE) == 0;
+    if (willHowl) {
+      this.setHowling(true);
       this.playSound(ModSounds.HUSKY_HOWL, HOWL_VOLUME, HOWL_PITCH);
       this.howlCooldownTicks = HOWL_COOLDOWN_TICKS;
+      this.howlActiveTicks = HOWL_DURATION_TICKS;
     }
+  }
+
+  @Override
+  public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {
+    super.registerControllers(controllers);
+    controllers.add(
+        new AnimationController<>(
+            this,
+            "howl",
+            0,
+            state -> {
+              if (this.isHowling()) {
+                if (this.isInSittingPose()) {
+                  return state.setAndContinue(RawAnimation.begin().thenLoop("howl_sit"));
+                }
+                return state.setAndContinue(RawAnimation.begin().thenLoop("howl"));
+              }
+              return PlayState.STOP;
+            }));
   }
 }
